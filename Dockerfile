1766f75ffca52e6652ae37ef85e7b92458b10d55
tuha <tuha@sapo.vn>
2024-01-25 15:49:50 +0700

    add docker file

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:11
-COPY target/getting-started-0.0.1-SNAPSHOT.jar app.jar
+COPY target/getting-started-service-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/main/java/com/example/gettingstartedservice/GettingStartedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gettingstartedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingStartedServiceApplication.class, args);
+    }
+
+}
